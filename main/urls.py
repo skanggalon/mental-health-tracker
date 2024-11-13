b872b72bfed65fffc7dd4c4c3fd7a45071e41aaa
z41d123@gmail.com <z41d123@gmail.com>
2024-11-13 16:21:38 +0700

    [Tutorial8] Done?

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,4 @@
-from main.views import show_main, create_mood_entry, show_xml, show_json, show_xml_by_id, show_json_by_id
+from main.views import create_mood_flutter, show_main, create_mood_entry, show_xml, show_json, show_xml_by_id, show_json_by_id
 from main.views import register, login_user, logout_user
 from main.views import edit_mood, delete_mood
 from django.urls import path
@@ -20,4 +20,5 @@ urlpatterns = [
     path('edit-mood/<uuid:id>', edit_mood, name='edit_mood'),
     path('delete/<uuid:id>', delete_mood, name='delete_mood'),
     path('create-mood-entry-ajax', add_mood_entry_ajax, name='add_mood_entry_ajax'),
+    path('create-flutter/', create_mood_flutter, name='create_mood_flutter'),
 ]
